3963e03c9072210cc2343c15f52efb165240f098
Bob Lutz <rjlutz@gmail.com>
2017-09-05 03:05:45 +0000

    saving WIP

--- clone.py
+++ clone.py
@@ -54,7 +54,7 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, ELU, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
@@ -76,16 +76,32 @@ from keras.layers.pooling import MaxPooling2D
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3))) # normalize and mean center
 model.add(Cropping2D(cropping=((70,25),(0,0))))
-model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
-model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
-model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
-model.add(Convolution2D(64,3,3,activation='relu'))
-model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(24,5,5,subsample=(2,2)))
+model.add(ELU())
+model.add(Dropout(0.5))
+
+model.add(Convolution2D(36,5,5,subsample=(2,2)))
+model.add(ELU())
+model.add(Dropout(0.5))
+
+model.add(Convolution2D(48,3,3,subsample=(2,2)))
+model.add(ELU())
+model.add(Dropout(0.5))
+
+model.add(Convolution2D(64,3,3,subsample=(2,2)))
+model.add(ELU())
+model.add(Dropout(0.5))
+
 model.add(Flatten())
+
 model.add(Dense(100))
+model.add(ELU())
 model.add(Dense(50))
+model.add(ELU())
 model.add(Dense(10))
+model.add(ELU())
 model.add(Dense(1))
+model.summary()
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.20, shuffle=True, nb_epoch=5)
