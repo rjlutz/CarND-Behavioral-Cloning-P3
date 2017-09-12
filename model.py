100f75b8d2825927dc5c3cf39d4e0cb7b315d7fd
Bob Lutz <rlutz@ggc.edu>
2017-09-12 13:20:05 -0400

    refinements

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 import csv
 import cv2
 import numpy as np
-from random import randint
+import random
 import matplotlib.pyplot as plt
+import sys;
 
 def add_driving_data(path, images, measurements):
 
@@ -10,56 +11,72 @@ def add_driving_data(path, images, measurements):
     # 0.1 = 0.043 radians, according to
     # https://hoganengineering.wixsite.com/randomforest/ \
     #         single-post/2017/03/13/Alright-Squares-Lets-Talk-Triangles
-    correction = 0.1; # parameter to tune
+    correction = 0.2; # parameter to tune
 
     with open(path + '/driving_log.csv') as csvfile:
         reader= csv.reader(csvfile);
         for line in reader:
             lines.append(line)
     for line in lines:
-        ##print(line);
         filenames = []
-        filenames.append(line[0].split('/')[-1])
-        filenames.append(line[1].split('/')[-1])
-        filenames.append(line[2].split('/')[-1])
+        for i in range(3):
+            filenames.append(line[i].split('/')[-1])
         steering_center = float(line[3])
         # create adjusted steering measurements for the side camera images
         steering_left = steering_center + correction
         steering_right = steering_center - correction
-        ## print("steering center {}".format(steering_center))
-        # steering info unhelpful if throttle is 0
-        ##if not (float(line[4]) < 0.001) and  \
-        ##    not (abs(steering_center) < 0.001 and randint(0,100) < 30):
-        if True:
 
-            # add image and angle
-            image_center = cv2.imread(path + '/IMG/' + filenames[0])
+        # steering info unhelpful if throttle is 0, DEAL WITH THIS??
+
+        # add image and angle
+        image_center = cv2.imread(path + '/IMG/' + filenames[0])
+
+        ## Straightest steering - undersample by 20%
+        images.append(image_center)
+        if (abs(steering_center) < 0.02 and random.random() <= 0.80):
+            if random.random() < 0.50:
+                images.append(image_center)
+                measurements.append(steering_center)
+            else:
+                images.append(np.fliplr(image_center)) # flip the center image, reverse angle
+                measurements.append(-steering_center)
+        # right turning, duplicate and tweak a little
+        if (steering_center >0.2 and steering_center <=0.5):
+            tweak = np.random.uniform(-1,1)/100.0
+            images.append(image_center)
+            measurements.append(steering_center * (1.0 + tweak))
+        # left turning
+        elif (steering_center >= -0.5 and steering_center < -0.2):
+            tweak = np.random.uniform(-1,1)/100.0
+            images.append(image_center)
+            measurements.append(steering_center * (1.0 + tweak))
+        else: ## calues between +/- (.02, .2) OR greater than +/- 0.5
             images.append(image_center)
             measurements.append(steering_center)
-            images.append(np.fliplr(image_center)) # flip the center image, reverse angle
-            measurements.append(-steering_center)
 
-            image_left = cv2.imread(path + '/IMG/' + filenames[1])
-            images.append(image_left)
-            measurements.append(steering_left)
-            images.append(np.fliplr(image_left)) # flip the left image, reverse angle
-            measurements.append(-steering_left)
+        # flip the lake-firstturn-repetitive image, reverse angle
+        image_left = cv2.imread(path + '/IMG/' + filenames[1])
+        images.append(image_left)
+        measurements.append(steering_left)
 
-            image_right = cv2.imread(path + '/IMG/' + filenames[2])
-            images.append(image_right)
-            measurements.append(steering_right)
-            images.append(np.fliplr(image_right)) # flip the right image, reverse angle
-            measurements.append(-steering_right)
+        # flip the right image, reverse angle
+        image_right = cv2.imread(path + '/IMG/' + filenames[2])
+        images.append(image_right)
+        measurements.append(steering_right)
 
     return (images, measurements)
 
-images, measurements = add_driving_data('./data/lake-dataCCW', [], [])
-images, measurements = add_driving_data('./data/lake-dataCW', images, measurements)
-## images, measurements = add_driving_data('./data/jungle-dataCCW', images, measurements)
-images, measurements = add_driving_data('./data/lake-dirtroad-turn-repetitive', images, measurements)
-images, measurements = add_driving_data('./data/lake-firstturn-repetitive', images, measurements)
-images, measurements = add_driving_data('./data/data-corrections', images, measurements)
-images, measurements = add_driving_data('./data/data-CCW-AJL', images, measurements)
+fnames = []
+fnames.extend(['./data/lake-dataCCW', './data/data-CCW-AJL']);
+fnames.extend(['./data/data-corrections', './data/lake-dirtroad-turn-repetitive'])
+##fnames.extend(['./data/lake-firstturn-repetitive', './data/lake-dataCW'])
+##fnames.append(['./data/jungle-dataCCW'])
+
+images = []
+measurements = []
+for f in fnames:
+    images, measurements = add_driving_data(f, images, measurements)
+    print("fname = {}, size = {}".format(f, len(images)))
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -69,41 +86,19 @@ from keras.layers import Flatten, Dense, Lambda, Cropping2D, ELU, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 
-# LeNET
-#model = Sequential()
- ## model.add(Flatten(input_shape=(160,320,3))) # orig, most simple
-#model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3))) # normalize and mean center
-#model.add(Cropping2D(cropping=((70,25),(0,0))))
-#model.add(Convolution2D(6,5,5,activation='relu'))
-#model.add(MaxPooling2D())
-#model.add(Convolution2D(6,5,5,activation='relu'))
-#model.add(MaxPooling2D())
-#model.add(Flatten())
-#model.add(Dense(120))
-#model.add(Dense(84))
-#model.add(Dense(1))
-
 # NVIDIA
 model = Sequential()
 model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160,320,3))) # normalize and mean center
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
-model.add(Dropout(0.5))
-
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
-model.add(Dropout(0.5))
-
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
-model.add(Dropout(0.5))
-
 model.add(Convolution2D(64,3,3,activation='relu'))
-model.add(Dropout(0.5))
-
 model.add(Convolution2D(64,3,3,activation='relu'))
-model.add(Dropout(0.5))
 
 model.add(Flatten())
+model.add(Dropout(0.5))
 
 model.add(Dense(100))
 model.add(Dense(50))
@@ -112,6 +107,11 @@ model.add(Dense(1))
 model.summary()
 
 model.compile(loss='mse', optimizer='adam')
+
+## backstop
+##sys.exit(0);
+
+
 history_object = model.fit(X_train, y_train, validation_split=0.20, shuffle=True, nb_epoch=5, \
     verbose=1)
 
