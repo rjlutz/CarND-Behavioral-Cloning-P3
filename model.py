999020dfeae88cd3fb05f66ee6a07e3866d1c999
Bob Lutz <rlutz@ggc.edu>
2017-09-17 09:02:51 -0400

    Model working here

--- model.py
+++ model.py
@@ -81,24 +81,6 @@ for f in focused:
     for i in range(times):
         observations = add_driving_data(f, observations)
 
-    # turns_file = dset + '/' +  'driving_log.csv'
-    #
-    # augmented_data = pd.read_csv(turns_file, header=0)
-    # augmented_data.columns = ["c_image", "l_image", "r_image", "steering", "throttle", "brake", "speed"]
-    # for i in range(times):
-    #     turns_list = []
-    #     for j in range(len(augmented_data)):
-    #         paths = []
-    #         paths.append(dset + '/IMG/' + augmented_data["c_image"][j].split('/')[-1]);
-    #         paths.append(dset + '/IMG/' + augmented_data["l_image"][j].split('/')[-1]);
-    #         paths.append(dset + '/IMG/' + augmented_data["r_image"][j].split('/')[-1]);
-    #         steer = augmented_data["steering"][j] * (1.0 + np.random.uniform(-1, 1) / 100.0)
-    #         turns_list.append([paths[0], paths[1], paths[2], steer])
-    #     observations += turns_list
-
-# print ("Turns/Running Total = {} {}. Turns added {} times".format( \
-#            len(turns_list), len(observations), times))
-
 random.shuffle(observations)
 
 from sklearn.model_selection import train_test_split
@@ -106,12 +88,6 @@ train_observations, validation_observations = train_test_split(observations, tes
 
 print("Training/Validation/Total Observations {} {} {}".format(len(train_observations), \
       len(validation_observations), len(train_observations) + len(validation_observations)))
-
-def cropScaleImage(image):
-    # shape = image.shape
-    # image = image[math.floor(60):shape[0]-20, 0:shape[1]]  # note: numpy arrays are (row, col)!
-    # image = cv2.resize(image,(new_size_col,new_size_row), interpolation=cv2.INTER_AREA)
-    return image
 
 def plotImageTrio(c, l, r, name, title):
     fig = plt.figure()
@@ -140,7 +116,7 @@ def plotRandomImage(dset, name, title):
 # visual validation of data set
 plotRandomImage(train_observations, 'example-camera-images.png', 'L/R/C')
 
-batch_size = 256
+batch_size = 128
 new_size_row = 64
 new_size_col = 64
 
@@ -161,9 +137,6 @@ def train_generator(samples, batch_size=batch_size):
                 center_image = cv2.cvtColor(cv2.imread(batch_sample[0]), cv2.COLOR_BGR2RGB)
                 left_image = cv2.cvtColor(cv2.imread(batch_sample[1]), cv2.COLOR_BGR2RGB)
                 right_image = cv2.cvtColor(cv2.imread(batch_sample[2]), cv2.COLOR_BGR2RGB)
-                center_image = cropScaleImage(center_image)
-                right_image = cropScaleImage(right_image)
-                left_image = cropScaleImage(left_image)
 
                 steering_center = float(batch_sample[3])
 
@@ -211,7 +184,7 @@ def train_generator(samples, batch_size=batch_size):
                 images.append(bright_image)
                 angles.append(angle)
 
-                # Randomly shadow image with 80% probability
+                # Randomly shadow image and add to images with 80% probability
                 ## inspiration from https://github.com/naokishibuya/car-behavioral-cloning/blob/master/utils.py
                 w = 320
                 h = 160
@@ -229,16 +202,6 @@ def train_generator(samples, batch_size=batch_size):
                 hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
                 hls[:, :, 1][cond] = hls[:, :, 1][cond] * s_ratio
                 shadow_image =  cv2.cvtColor(hls, cv2.COLOR_HLS2RGB)
-                # shear_range = 40
-                # rows, cols, channels = image.shape
-                # dx = np.random.randint(-shear_range, shear_range + 1)
-                # random_point = [cols / 2 + dx, rows / 2]
-                # pts1 = np.float32([[0, rows], [cols, rows], [cols / 2, rows / 2]])
-                # pts2 = np.float32([[0, rows], [cols, rows], random_point])
-                # dsteering = dx / (rows / 2) * 360 / (2 * np.pi * 25.0) / 10.0
-                # M = cv2.getAffineTransform(pts1, pts2)
-                # shear_image = cv2.warpAffine(center_image, M, (cols, rows), borderMode=1)
-                # shear_angle = angle + dsteering
                 if random.random() <= 0.80:
                     images.append(shadow_image)
                     angles.append(angle)
@@ -265,7 +228,6 @@ def valid_generator(samples, batch_size=batch_size):
                 #Validation generator: use center images only, no augmentation
                 for batch_sample in batch_samples:
                     center_image = cv2.cvtColor(cv2.imread(batch_sample[0]), cv2.COLOR_BGR2RGB)
-                    center_image = cropScaleImage(center_image)
                     images.append(center_image)
                     angles.append(float(batch_sample[3]))
                 X_train = np.array(images)
@@ -280,110 +242,51 @@ from keras.layers import Flatten, Dense, Lambda, ELU, Dropout, Activation
 from keras.layers.convolutional import Convolution2D, Cropping2D, ZeroPadding2D, MaxPooling2D
 import tensorflow as tf
 tf.python.control_flow_ops = tf
-from keras.optimizers import Adam
+##from keras.optimizers import Adam
 
-# # Function to resize image
-# def resize_image(image):
-#     import tensorflow as tf
-#     return tf.image.resize_images(image,[new_size_row,new_size_col])
-#
-# ## try this model
-# filter_size = 3
-# pool_size = (2,2)
-#
-# model = Sequential() ## Vivek
-# model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(new_size_row, new_size_col, 3))) # normalize and mean center
-# # Resise data within the neural network
-# model.add(Convolution2D(3,1,1, border_mode='valid',name='conv0', init='he_normal'))
-# model.add(ELU())
-# model.add(Convolution2D(32,filter_size,filter_size,border_mode='valid',name='conv1', init='he_normal'))
-# model.add(ELU())
-# model.add(Convolution2D(32,filter_size,filter_size,border_mode='valid',name='conv2', init='he_normal'))
-# model.add(ELU())
-# model.add(MaxPooling2D(pool_size=pool_size))
-# model.add(Dropout(0.5))
-# model.add(Convolution2D(64,filter_size,filter_size,border_mode='valid',name='conv3', init='he_normal'))
-# model.add(ELU())
-# model.add(Convolution2D(64,filter_size,filter_size,border_mode='valid',name='conv4', init='he_normal'))
-# model.add(ELU())
-# model.add(MaxPooling2D(pool_size=pool_size))
-# model.add(Dropout(0.5))
-# model.add(Convolution2D(128,filter_size,filter_size,border_mode='valid',name='conv5', init='he_normal'))
-# model.add(ELU())
-# model.add(Convolution2D(128,filter_size,filter_size,border_mode='valid',name='conv6', init='he_normal'))
-# model.add(ELU())
-# model.add(MaxPooling2D(pool_size=pool_size))
-# model.add(Dropout(0.5))
-#
-# model.add(Flatten())
-#
-# model.add(Dense(512,name='hidden1', init='he_normal'))
-# model.add(ELU())
-# model.add(Dropout(0.5))
-# model.add(Dense(64,name='hidden2', init='he_normal'))
-# model.add(ELU())
-# model.add(Dropout(0.5))
-# model.add(Dense(16,name='hidden3',init='he_normal'))
-# model.add(ELU())
-# model.add(Dropout(0.5))
-# model.add(Dense(1, name='output', init='he_normal'))
-#
-# i##--##adam = Adam(lr=1e-4, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-# model.compile(optimizer='adam', loss='mse')
-
-
-# NVIDIA
-# model = Sequential()
-#
-# model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160,320,3))) # normalize and mean center
-# model.add(Cropping2D(cropping=((70,25),(0,0))))
-#
-# # Resise data within the neural network
-# model.add(Lambda(resize_image))
-#
-# model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
-# model.add(Dropout(0.5))
-# model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
-# model.add(Dropout(0.5))
-# model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
-# model.add(Dropout(0.5))
-# model.add(Convolution2D(64,3,3,activation='relu'))
-# model.add(Dropout(0.5))
-# model.add(Convolution2D(64,3,3,activation='relu'))
-# model.add(Dropout(0.5))
-#
-# model.add(Flatten())
-# model.add(Dropout(0.5))
-#
-# model.add(Dense(100))
-# model.add(Dense(50))
-# model.add(Dense(10))
-# model.add(Dense(1))
-# model.compile(loss='mse', optimizer='adam')
-
+# NVIDIA, modified, inspiration from https://github.com/naokishibuya/car-behavioral-cloning
 model = Sequential()
-model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160, 320, 3))) # normalize and mean center
+
+model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160,320,3))) # normalize and mean center
 model.add(Cropping2D(cropping=((60,25),(0,0))))
-model.add(Convolution2D(8, 5, 5, border_mode='valid', activation='tanh')) # -> (66,316,8)
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation="elu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation="elu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation='elu'))
+model.add(Convolution2D(64,3,3,activation='elu'))
+model.add(Convolution2D(64,3,3,activation='elu'))
+
+model.add(Flatten())
 model.add(Dropout(0.5))
-model.add(Convolution2D(16, 5, 5, border_mode='valid', activation='tanh', subsample=(2,2))) # -> (31,156,16)
-model.add(Dropout(0.5))
-model.add(Convolution2D(20, 5, 5, border_mode='valid', activation='tanh', subsample=(2,2))) # -> (14,76,20)
-model.add(Dropout(0.5))
-model.add(Convolution2D(24, 5, 5, border_mode='valid', activation='tanh', subsample=(1,2))) # -> (10,36,24)
-model.add(Dropout(0.5))
-model.add(Convolution2D(24, 5, 5, border_mode='valid', activation='tanh', subsample=(1,2))) # -> (6,16,24)
-model.add(Dropout(0.5))
-model.add(Flatten()) # 6x16x24 -> 2304
-from keras.regularizers import l2
-model.add(Dense(30, activation='tanh', W_regularizer=l2(0.01)))
-model.add(Dropout(0.4))
-model.add(Dense(25, activation='tanh', W_regularizer=l2(0.01)))
-model.add(Dropout(0.3))
-model.add(Dense(20, activation='tanh', W_regularizer=l2(0.01)))
-model.add(Dropout(0.2))
-model.add(Dense(1, activation='tanh', W_regularizer=l2(0.01)))
-model.compile(loss='mse', optimizer='adam')
+
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+model.compile(loss='mse', optimizer=Adam(lr=1.0e-4))
+
+# model = Sequential()
+# model.add(Lambda(lambda x: x / 127.5 - 1.0, input_shape=(160, 320, 3))) # normalize and mean center
+# model.add(Cropping2D(cropping=((60,25),(0,0))))
+# model.add(Convolution2D(8, 5, 5, border_mode='valid', activation='tanh')) # -> (66,316,8)
+# model.add(Dropout(0.5))
+# model.add(Convolution2D(16, 5, 5, border_mode='valid', activation='tanh', subsample=(2,2))) # -> (31,156,16)
+# model.add(Dropout(0.5))
+# model.add(Convolution2D(20, 5, 5, border_mode='valid', activation='tanh', subsample=(2,2))) # -> (14,76,20)
+# model.add(Dropout(0.5))
+# model.add(Convolution2D(24, 5, 5, border_mode='valid', activation='tanh', subsample=(1,2))) # -> (10,36,24)
+# model.add(Dropout(0.5))
+# model.add(Convolution2D(24, 5, 5, border_mode='valid', activation='tanh', subsample=(1,2))) # -> (6,16,24)
+# model.add(Dropout(0.5))
+# model.add(Flatten()) # 6x16x24 -> 2304
+# from keras.regularizers import l2
+# model.add(Dense(30, activation='tanh', W_regularizer=l2(0.01)))
+# model.add(Dropout(0.4))
+# model.add(Dense(25, activation='tanh', W_regularizer=l2(0.01)))
+# model.add(Dropout(0.3))
+# model.add(Dense(20, activation='tanh', W_regularizer=l2(0.01)))
+# model.add(Dropout(0.2))
+# model.add(Dense(1, activation='tanh', W_regularizer=l2(0.01)))
+# model.compile(loss='mse', optimizer='adam')
 
 model.summary()
 
